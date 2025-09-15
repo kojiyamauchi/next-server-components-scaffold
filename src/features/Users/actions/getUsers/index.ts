6277723f34af1585b20750bab271ac56a93cacf6
Koji <all.the.small.things@me.com>
2025-09-16 02:27:15 +0900

    modify.

--- src/features/Users/actions/getUsers/index.ts
+++ src/features/Users/actions/getUsers/index.ts
@@ -1,12 +1,16 @@
 'use server'
 
 import { User } from '@prisma/client'
+import { revalidatePath } from 'next/cache'
+
+import { pagesPath } from '@/libs'
 
 import { fetchUsersRepo } from '../../repositories'
 
 export const getUsers = async (): Promise<User[]> => {
   try {
     const users = await fetchUsersRepo()
+    revalidatePath(pagesPath.users.$url().path)
     return users
   } catch (error) {
     console.error(`Maybe Repository Layer Error: ${error}`)
