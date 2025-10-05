32a56cf92d9cbab16c35ed2ea5e74609e923d7b8
Koji <all.the.small.things@me.com>
2025-10-05 19:14:05 +0900

    fix review - Comments suppressed due to low confidence (1): https://github.com/kojiyamauchi/next-server-components-scaffold/pull/19#pullrequestreview-3302308375

--- src/components/LoadingForm/index.tsx
+++ src/components/LoadingForm/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const LoadingForm: React.FC<Props> = ({ label }) => {
   return (
     <dl className="flex flex-col items-start justify-start w-full">
-      <dt className="flex items-start justify-start text-[18px] font-bold w-full pb-[6px]">{label}</dt>
+      {label && <dt className="flex items-start justify-start text-[18px] font-bold w-full pb-[6px]">{label}</dt>}
       <dd className="flex items-start justify-start w-full">
         <Skeleton count={1} height={35} duration={0.4} containerClassName="w-full md:max-w-[400px]" />
       </dd>
