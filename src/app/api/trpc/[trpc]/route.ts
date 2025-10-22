6edab1b5a97dbd6cec6a5cc4b5ff9a47400c3197
Koji <all.the.small.things@me.com>
2025-10-22 18:17:15 +0900

    add trpc on error.

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -10,6 +10,24 @@ const handler = (req: Request): Promise<Response> =>
     createContext: () => {
       return { exampleContext: true }
     },
+    onError({ error, type, path, input, ctx, req }) {
+      console.error('error: ', error)
+      console.info('type: ', type)
+      console.info('path: ', path)
+      console.info('input: ', input)
+      console.info('context: ', ctx)
+      console.info('request: ', req)
+    },
+    /*
+    {
+      error: TRPCError; // the original error
+      type: 'query' | 'mutation' | 'subscription' | 'unknown';
+      path: string | undefined; // path of the procedure that was triggered
+      input: unknown;
+      ctx: Context | undefined;
+      req: BaseRequest; // request object
+    }
+    */
   })
 
 export { handler as GET, handler as POST }
